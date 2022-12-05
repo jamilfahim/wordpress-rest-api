b789429e0d946c98dc204ee8ad06ac3ae249f4e6
Jamil Hossain Fahim <45314233+jamilfahim@users.noreply.github.com>
2022-12-05 13:18:00 +0600

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,25 @@
+/**
+ * WordPress rest api
+ *
+ */
+add_action('rest_api_init', function(){
+
+	register_rest_route('softtechit/v1', '/posts/(?P<id>\d+)', array( 
+		'methods' => 'GET',
+		'callback' => function( WP_REST_Request $request){
+	
+			$id =$request->get_param('id');
+			$args = array(
+				'post_type' => 'post',
+				'post_status' => 'publish',
+				'p' => $id,   // id of the post you want to query
+		  );
+			$query = new WP_Query( $args );
+
+			return $query;
+	
+		}
+	));
+
+
+});
